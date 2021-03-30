bf3bbaa535f5f83ab6fb754b24806c001a3c1757
Nia <40437931+Romnia@users.noreply.github.com>
2021-03-30 16:23:29 -0500

    Adding a development local settings

---
+++ gosenzo_reader/settings/local_dev.py
@@ -0,0 +1,51 @@
+import os
+
+from .base import *
+
+
+dev_domain = os.environ.get("DEV_CANONICAL_ROOT_DOMAIN", "test.shamiko.moe")
+
+CANONICAL_ROOT_DOMAIN = dev_domain
+
+DEBUG = True
+
+ALLOWED_HOSTS = [dev_domain, "shamiko.moe", "www.shamiko.moe", "localhost", *]
+
+CANONICAL_SITE_NAME = dev_domain
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "file": {
+            "level": "WARNING",
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "verbose",
+            "filename": os.path.join(PARENT_DIR, "gosenzo_reader.log"),
+            "maxBytes": 1024 * 1024 * 100,  # 100 mb
+        }
+    },
+    "loggers": {
+        "django": {"handlers": ["file"], "level": "WARNING", "propagate": True,},
+    },
+}
+
+CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache",}}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DEV_DB_NAME"),
+        "USER": os.environ.get("DEV_DB_USER"),
+        "PASSWORD": os.environ.get("DEV_DB_PASS"),
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
+
+OCR_SCRIPT_PATH = os.path.join(PARENT_DIR, "ocr_tool.sh")
